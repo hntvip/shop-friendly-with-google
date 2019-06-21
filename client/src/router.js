6e9b8c3ed2c47171df26ee5b4d11b6d7c6786073
Tai Phan <tai@sensolus.com>
2019-06-21 11:39:44 +0700

    add product list and router

--- client/src/router.js
+++ client/src/router.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
-
+import ProductPage from './views/ProductPage.vue'
 Vue.use(Router)
 
 export default new Router({
@@ -10,6 +10,11 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/product-page',
+      name: 'product-page',
+      component: ProductPage
     },
     {
       path: '/about',
---
+++ client/src/views/ProductPage.vue
@@ -0,0 +1,14 @@
+<template>
+  <Product></Product>
+</template>
+
+<script>
+// @ is an alias to /src
+import Product from '@/components/Product.vue'
+export default {
+  name: 'product-page',
+  components: {
+    Product
+  }
+}
+</script>
