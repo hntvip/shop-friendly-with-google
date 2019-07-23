087c7b34704ba2500af7125fbccf71c2b6a88b4f
Tai Phan <tai@sensolus.com>
2019-07-23 10:52:10 +0700

    TICKET- 41: push missing files

--- client/src/router.js
+++ client/src/router.js
@@ -1,59 +1,84 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Layout from './layout/index.vue' 
-import ProductListPage from './views/ProductListPage.vue'
-import ProductPage from './views/ProductPage.vue'
+
 Vue.use(Router)
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      component: Layout,
-      redirect: '/dashboard',
-      children: [
-        {
-          path: 'dashboard',
-          component: () => import('@/views/dashboard/index'),
-          name: 'Dashboard'
+import Layout from './layout/index.vue' 
+
+import ProductListPage from './views/ProductListPage.vue'
+
+/**
+ * constantRoutes
+ * a base page that does not have permission requirements
+ * all roles can be accessed
+ */
+
+export const constantRoutes = [
+  {
+    path: '/',
+    component: Layout,
+    redirect: '/dashboard',
+    children: [
+      {
+        path: 'dashboard',
+        component: () => import('@/views/dashboard/index'),
+        name: 'Dashboard',
+        meta: {
+          icon: 'el-icon-location',
+          title: 'Dashboard'
         }
-      ]
-    },
-    {
-      component: Layout,
-      path: '/product-list-page',
-      name: 'product-list-page',
-      children: [
-        {
-          path: 'index',
-          component: ProductListPage
-        }
-      ]
-    },
-    {
-      component: Layout,
-      path: '/product-page',
-      name: 'product-page',
-      children: [
-        {
-          path: 'index',
-          component: ProductPage
-        }
-      ]
-    },
-    {
-      component: Layout,
-      path: '/about',
-      name: 'about',
-      children: [
-        {
-          path: 'index', 
-          // route level code-splitting
-          // this generates a separate chunk (about.[hash].js) for this route
-          // which is lazy-loaded when the route is visited.
-          component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-        }
-      ]
-    }
-  ]
+      }
+    ]
+  },
+  {
+    component: Layout,
+    path: '/giay-cao-got-nu',
+    name: 'giay-cao-got-nu',
+    children: [
+      {
+        path: 'index',
+        component: ProductListPage,
+        meta: {
+          icon: 'el-icon-location',
+          title: 'Tabs'
+        },
+      }
+    ]
+  },
+  {
+    component: Layout,
+    path: '/about',
+    name: 'about',
+    children: [
+      {
+        meta: {
+          icon: 'el-icon-location',
+          title: 'About'
+        },
+        path: 'index', 
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      }
+    ]
+  }
+]
+
+/**
+ * asyncRoutes
+ * the routes that need to be dynamically loaded based on user roles
+ */
+
+export const asyncRoutes = [
+
+]
+
+const createRouter = () => new Router({
+  routes: constantRoutes
 })
+
+const router = createRouter();
+
+
+export default router
